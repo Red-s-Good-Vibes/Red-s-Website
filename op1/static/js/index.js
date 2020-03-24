708760142c9a7ae3c97129f8a6b490c2d7493657
Matt-the-Marxist <m.morrissette105000@gmail.com>
2020-03-24 14:10:23 -0400

    so much work

--- op1/static/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(()=>{
-    $(".op1").click(()=>{
-        window.location.assign("#WhatIsRedsGoodVibes")
-    })
-    $(".op2").click(()=>{
-        window.location.assign("#AboutUs")
-    })
-})
